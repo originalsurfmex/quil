a96cfb9b235dcf1230c07cd82bee84556175a88a
anon anon <delgado.raymond@gmail.com>
2014-01-09 21:20:31 -0500

    processing 2.1

--- project.clj
+++ project.clj
@@ -6,11 +6,30 @@
                  :post "clj-processing@googlegroups.com"}
   :license {:name "Common Public License - v 1.0"
             :url "http://www.opensource.org/licenses/cpl1.0"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojars.processing-core/org.processing.core "1.5.1"]
-                 [org.clojars.processing-core/org.processing.gluegen-rt "1.5.1"]
-                 [org.clojars.processing-core/org.processing.jogl "1.5.1"]
-                 [org.clojars.processing-core/org.processing.opengl "1.5.1"]
-                 [org.clojars.processing-core/org.processing.itext "1.5.1"]
-                 [org.clojars.processing-core/org.processing.pdf "1.5.1"]]
+
+    :dependencies [[org.clojure/clojure "1.5.1"]
+                 [processing/core "2.1"]
+                 [processing/jogl-all "2.1"]
+                 [processing/jogl-all-natives-linux-amd64 "2.1"]
+                 [processing/jogl-all-natives-linux-i586 "2.1"]
+                 [processing/jogl-all-natives-linux-armv6hf "2.1"]
+                 [processing/jogl-all-natives-macosx-universal "2.1"]
+                 [processing/jogl-all-natives-windows-amd64 "2.1"]
+                 [processing/jogl-all-natives-windows-i586 "2.1"]
+                 [processing/gluegen-rt "2.1"]
+                 [processing/gluegen-rt-natives-linux-amd64 "2.1"]
+                 [processing/gluegen-rt-natives-linux-i586 "2.1"]
+                 [processing/gluegen-rt-natives-linux-armv6hf "2.1"]
+                 [processing/gluegen-rt-natives-macosx-universal "2.1"]
+                 [processing/gluegen-rt-natives-windows-amd64 "2.1"]
+                 [processing/gluegen-rt-natives-windows-i586 "2.1"] ]
+  :repositories [["processinglibs" "https://github.com/originalsurfmex/originalsurfmex_m2/tree/master/repository/"]]
   :aot [quil.applet])
+
+;;  :dependencies [[org.clojure/clojure "1.4.0"]
+;;                 [org.clojars.processing-core/org.processing.core "1.5.1"]
+;;                 [org.clojars.processing-core/org.processing.gluegen-rt "1.5.1"]
+;;                 [org.clojars.processing-core/org.processing.jogl "1.5.1"]
+;;                 [org.clojars.processing-core/org.processing.opengl "1.5.1"]
+;;                 [org.clojars.processing-core/org.processing.itext "1.5.1"]
+;;                 [org.clojars.processing-core/org.processing.pdf "1.5.1"]]
